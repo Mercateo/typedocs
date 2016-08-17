b3c28620dd807675f3fe46a53c0dca7cb246ddc1
Alexander Girke <alexander.girke@mercateo.com>
2016-08-17 10:44:55 +0200

    edited tests

--- tests/analysis/property-check.ts
+++ tests/analysis/property-check.ts
@@ -1,3 +1,20 @@
 /**
  * Created by alexander on 11.08.16.
  */
+import { isNodeExported } from '../../src/analysis/property-check';
+import expect from "expect";
+import {
+  NodeFlags,
+  SyntaxKind,
+} from 'typescript';
+
+describe('test property-check', () => {
+  it('isNodeExported', () => {
+    expect(isNodeExported({
+      flags: NodeFlags.Export,
+      parent: {
+        kind: SyntaxKind.SourceFile
+      }
+    } as any)).toBeTruthy();
+  });
+});
--- tests/unit.ts
+++ tests/unit.ts
@@ -1,3 +1,6 @@
 /**
  * Created by alexander on 11.08.16.
  */
+
+import './analysis/property-check';
+import './markdown/component-gen';
